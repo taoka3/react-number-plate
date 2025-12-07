0c59383372c88ee8e2cbe1e52c3fa3019dc121e2
taoka3 <mail@zip358.com>
2025-12-07 11:14:07 +0900

    ダウンロードボタンの機能を改善し、選択されたオプションを画像に反映させるフィルターを追加

--- src/DownloadButton.tsx
+++ src/DownloadButton.tsx
@@ -9,8 +9,25 @@ const DownloadButton = ({ targetRef }: Props) => {
   const handleDownload = async () => {
     if (!targetRef.current) return;
     
-    toPng(targetRef.current)
-        .then((dataUrl) => {
+    toPng(targetRef.current,{
+      filter: (n) => {
+        if (n instanceof HTMLSelectElement) {
+
+          const currentValue = n.value;
+
+          Array.from(n.options).forEach((opt) => {
+            if (opt.value === currentValue) {
+              opt.setAttribute("selected", "selected");
+            } else {
+              opt.removeAttribute("selected");
+            }
+          });
+          return true;
+        }
+
+        return true;
+      }
+    }).then((dataUrl) => {
         const link = document.createElement('a');
         link.download = 'NumberPlate.png';
         link.href = dataUrl;
